d7d9eb2ded039a6ff74df02a59717462b671e4f9
Locchan <locchansky@gmail.com>
2024-06-03 20:58:44 +0300

    lodg: Cross-compilation fixed

--- lodg/src/gatherers/gath_unix.c
+++ lodg/src/gatherers/gath_unix.c
@@ -2,6 +2,18 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// Unix gatherers are obviously unix-specific
+//  debug-build will put test data in build directory
+//  so that the gatherers can be debugged on non-unix OSs
+
+#ifdef __unix__
+char* loadavg_path = "/proc/loadavg";
+char* meminfo_path = "/proc/meminfo";
+#elif defined(_WIN32) || defined(WIN32)
+char* loadavg_path = "./loadavg";
+char* meminfo_path = "./meminfo";
+#endif
+
 float get_la1(){
     // cpu  1417 0 2678 73606 0 0 145 0 0 0
     FILE *fp;
